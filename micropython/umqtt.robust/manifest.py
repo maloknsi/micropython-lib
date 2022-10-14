1a5f25b0085aced0450c31d6943181b49c30c297
Jim Mussared <jim.mussared@gmail.com>
2022-10-14 16:53:19 +1100

    umqtt.robust/manifest.py: Add dependency on umqtt.simple.
    This was missed when the manifest.py files were added.
    
    Signed-off-by: Jim Mussared <jim.mussared@gmail.com>

--- micropython/umqtt.robust/manifest.py
+++ micropython/umqtt.robust/manifest.py
@@ -1,7 +1,8 @@
 metadata(
-    description='Lightweight MQTT client for MicroPython ("robust" version).', version="1.0.2"
+    description='Lightweight MQTT client for MicroPython ("robust" version).', version="1.0.3"
 )
 
 # Originally written by Paul Sokolovsky.
 
+require("umqtt.simple")
 package("umqtt")
